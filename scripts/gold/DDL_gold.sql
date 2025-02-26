8e86a23044d27cd1ec6dfb4d29e68047c7aa103a
Mahesh M <160126376+XTR2k4@users.noreply.github.com>
2025-02-26 22:05:46 +0530

    Update DDL_gold.sql

--- scripts/gold/DDL_gold.sql
+++ scripts/gold/DDL_gold.sql
@@ -32,9 +32,9 @@ SELECT
 	ci.cst_create_date AS create_date
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
-ON 		  ci.cst_key = ca.cid
+	ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
-ON 		  ci.cst_key = la.cid;
+	ON ci.cst_key = la.cid;
 
 -- =============================================================================
 -- Create Dimension: gold.dim_products
@@ -54,7 +54,7 @@ SELECT
 	pn.prd_start_dt AS start_date
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
-ON pn.cat_id = pc.id
+	ON pn.cat_id = pc.id
 WHERE pn.prd_end_dt IS NULL;
 
 -- =============================================================================
@@ -73,6 +73,6 @@ SELECT
   sd.sls_price AS price
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
-ON sd.sls_prd_key = pr.product_number
+	ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
-ON sd.sls_cust_id = cu.customer_id;
+	ON sd.sls_cust_id = cu.customer_id;
